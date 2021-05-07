a10620c8f29b8409204637c7ee8bd9d0f96ff0ab
reve23 <78494847+reve23@users.noreply.github.com>
2021-05-07 16:30:26 +0600

    day1

---
+++ day1.py
@@ -0,0 +1,53 @@
+#introduction
+print("Hello World!")
+print("A")
+print("A")
+print("A")
+print("A")
+print("A")
+print(23)
+print(23.768)
+print(True)
+print(False)
+print("##################################################################")
+
+
+#Variables
+x = 234565466246562676746767667666766
+X = 0
+y = 334626467656747266465782646825662456646872642786786
+name = "siam"
+bool = True
+print(name)
+print(bool)
+print(x)
+print(X)
+print(y)
+print(x,y,X)
+_name = "tim"
+print(_name)
+
+
+#rules of naming variables
+#1)Don't start with numbers
+#2)don't use special characters
+#3)don't use highphen
+print("##################################################################")
+
+#Data Types
+
+#strings are srrounded by "" or ''
+#ints are full number like 0,1,2,3,4,5,6,7,8,10,,100,1000,17893 etc
+#floats are numbers with fraction like 1.24, 7.89, 10.5 etc
+#booleans are True or False value
+
+
+
+
+
+
+
+
+
+
+
