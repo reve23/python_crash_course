4665bd286f136f4752d8d3af3fcef4ff29e733c6
reve23 <78494847+reve23@users.noreply.github.com>
2021-05-09 16:07:35 +0600

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,59 @@
+#basic operators
+
+print(12 + 2)
+print(13 - 12)
+print(14 * 2)
+print(24 / 12)
+print(24 // 12)
+print(23 ** 2)
+print(17 % 9)
+
+age = 16
+age2 = 17
+
+print(age + age2)
+print(age - age2)
+print(age * age2)
+print(age / age2)
+print(age // age2)
+print(age ** age2)
+print(age % age2) 
+
+#inputs
+
+#convertion:
+#string => int {
+
+#int(input())
+
+#}
+#string => float {
+
+#float(input())
+
+#}
+
+name = input("What is your Name? ")
+age = int(input("What is your age: "))
+print(name , age)
+
+#Make a hospital form where you have to fill few perameters. The perameters are
+#name, age, address, phone_number, email..
+#This should identify your information as :
+# Your name is {name} and you are {age} years old. Your address is {address}
+#We'll call you on {phone_number} and we've sent an email to {email}..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
