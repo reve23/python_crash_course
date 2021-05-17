18e6823f6f03da816991646ede4e592209c27e3c
reve23 <78494847+reve23@users.noreply.github.com>
2021-05-17 15:23:46 +0600

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,4 @@
+#Question
+#Your user is a person who wants a bank loan. Now we can only give lones if the
+#user doesn't have any criminal record and his/her yearly salary is more than
+#120k USD. Now check if the conditions are true and give the loan.
