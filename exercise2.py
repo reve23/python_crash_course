3300b666dbd50995cea1ed29598d9c30c7f4ec7f
reve23 <78494847+reve23@users.noreply.github.com>
2021-05-17 15:15:15 +0600

    Add files via upload

--- exercise2.py
+++ exercise2.py
@@ -5,3 +5,14 @@
 #Your answer should look like :
 #You are 18 years old so you can drive,Have a good Journey
 #or, You can't drive
+
+#Answer:
+
+age = int(input("Enter your age: "))
+
+if age == 18:
+    print("You are 18 years old so you can drive,Have a good Journey")
+elif age > 18:
+    print("You are more than 18 years old so you can drive,Have a good Journey")
+else:
+    print("You can't drive")
